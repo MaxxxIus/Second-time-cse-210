674d26a55d93bb4e7b8d2f4f8998a17dd90019de
Maxxxlus <Hatineer@gmail.com>
2026-05-11 17:12:10 -0600

    finsihed assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string grade = " ";
+        Console.Write("What is your Grade percentage? ");
+        float score = float.Parse(Console.ReadLine());
+        if (score >= 90)
+        {
+            grade = "A";
+        } else if (score >= 80)
+        {
+            grade = "B";
+        } else if (score >= 70)
+        {
+            grade = "C";
+        } else if (score >= 60)
+        {
+            grade = "D";
+        } else
+        {
+          grade = "f";  
+        }
+
+        Console.WriteLine($"Your Grade: {grade}");
+        if (score >= 70)
+        {
+            Console.WriteLine("Congrates!!! You passed!!");
+        } else
+        {
+            Console.WriteLine("Too Bad. See you next semester!!!");
+        }
     }
 }
