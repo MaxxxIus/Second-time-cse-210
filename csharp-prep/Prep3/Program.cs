9ffc0703b8765b68dad7ea475d49052c9de024f0
Maxxxlus <Hatineer@gmail.com>
2026-05-11 23:31:33 -0600

    finsihed assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGen = new Random();
+        int magicNum = randomGen.Next(1, 100);
+
+        bool ansCheck = false;
+        do
+        {
+
+            Console.Write("Guess the number: ");
+            int guess = int.Parse(Console.ReadLine());
+
+            if (guess == magicNum)
+            {
+                Console.WriteLine("Thats Correct!!!");
+                ansCheck = true;
+            } else if (guess <= magicNum)
+            {
+                Console.WriteLine("Incorrect, guess Higher!!!");
+            } else if (guess >= magicNum)
+            {
+                Console.WriteLine("Incorrect, Guess Lower!!");
+            }
+        } while (ansCheck != true);
     }
 }
