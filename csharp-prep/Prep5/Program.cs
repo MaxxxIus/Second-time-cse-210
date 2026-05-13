be7b7eb1cd577e78a3b4b9dbc31e008bd68faeb6
Maxxxlus <Hatineer@gmail.com>
2026-05-12 21:40:56 -0600

    finished assignment

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome!!!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("What is your name?: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("What is your number?: ");
+            int number = int.Parse(Console.ReadLine());
+            return number;
+        }
+
+        static void PromptUserBirthday(out int birthday)
+        {
+            Console.Write("What is your birthday?" );
+            birthday = int.Parse(Console.ReadLine());
+        }
+
+        static int SquareNum(int num)
+        {
+            int newNum = num * num;
+            return newNum;
+        }
+
+        static void DisplayResaults(string name, int num, int birthYear)
+        {
+            int age = 2026 - birthYear;
+            Console.WriteLine($"{name}, your number squared is {num}!!!");
+            Console.WriteLine($"{name}, You are {age} years old as of 2026!!!");
+        }
+
+        DisplayWelcome();
+        string name = PromptUserName();
+        int num = PromptUserNumber();
+        int birthday;
+        PromptUserBirthday(out birthday);
+        int numSquared = SquareNum(num);
+        DisplayResaults(name, numSquared, birthday);
     }
 }
