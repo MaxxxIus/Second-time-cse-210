609d132ce7ab023790a3577e71442c49aef98836
Maxxxlus <Hatineer@gmail.com>
2026-06-02 13:18:08 -0600

    assignment done

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssignment ass1 = new MathAssignment("John", "math", "7.3", "8-19");
+        Console.WriteLine(ass1.GetSummary());
+        Console.WriteLine(ass1.GetHomeworkList());
+
+        WrittingAssignment ass2 = new WrittingAssignment("Mike Smite", "English History", "The End of the AMercan Revolution");
+        Console.WriteLine(ass2.GetSummary());
+        Console.WriteLine(ass2.GetWritingInfo());
     }
 }
